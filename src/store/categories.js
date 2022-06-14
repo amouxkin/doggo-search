aef2a6b00fca05c3caa3c028a0975777c7644d2d
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-15 00:55:51 +0530

    refactor(queries-categories): remove store

--- src/store/categories.js
+++ src/store/categories.js
@@ -1,10 +1,5 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext } from 'react'
 
 export const CategoriesContext = createContext(null)
 
 export const useCategoriesStore = () => useContext(CategoriesContext)
-
-export const CategoriesStore = (props) => {
-  const [categories, setCategories] = useState(undefined)
-  return <CategoriesContext.Provider value={{ categories, setCategories }} {...props}/>
-}
