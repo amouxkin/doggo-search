3d6857354ea685c00f870e1881b03936a0f4ea04
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-15 00:39:18 +0530

    feat(queries-categories): concat and return an array for each breed category

--- src/queries/categories.js
+++ src/queries/categories.js
@@ -5,5 +5,11 @@ export const fetchCategories = async () => {
     `${process.env.REACT_APP_API_URL}/breeds/list/all`
   )
 
-  return data.message
+  return Object.entries(data.message).reduce((previousValue, [breed, subBreeds]) => {
+    // Pushes the breed name
+    previousValue.push(breed)
+    // Pushes all the sub-breed
+    if (!!subBreeds.length) subBreeds.forEach(subBreed => previousValue.push(`${breed}/${subBreed}`))
+    return previousValue
+  }, [])
 }
