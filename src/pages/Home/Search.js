abbb12dbfb29f22c834022996254ecde562fca80
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-15 11:38:45 +0530

    refactor(pages--home): move state of selected categories to top level context

--- src/pages/Home/Search.js
+++ src/pages/Home/Search.js
@@ -1,23 +1,21 @@
-import { useState } from 'react'
 import { Button, Col, Row, Select } from 'antd'
 import { useQuery } from 'react-query'
 import { fetchImages } from '../../queries'
 import { useCategoriesStore } from '../../store/categories'
 
 export const Search = () => {
-  const categories = useCategoriesStore()
-  const [value, setValue] = useState([])
-  const { refetch, isFetching } = useQuery('images', async () => await fetchImages(value), { enabled: false })
+  const { allCategories, setSelectedCategories, selectedCategories } = useCategoriesStore()
+  const { refetch, isFetching } = useQuery('images', async () => await fetchImages(selectedCategories), { enabled: false })
 
   return <Row justify={'center'}>
     <Col>
-      <Select style={{ width: 300 }} value={value}
-              onChange={newValues => newValues.length <= 5 && setValue(newValues)}
-              options={categories} mode={'multiple'}
+      <Select style={{ width: 300 }} value={selectedCategories}
+              onChange={newValues => newValues.length <= 5 && setSelectedCategories(newValues)}
+              options={allCategories} mode={'multiple'}
               placeholder={'Search'}/>
     </Col>
     <Col>
-      <Button disabled={!value.length || isFetching}
+      <Button disabled={!selectedCategories.length || isFetching}
               onClick={refetch}>Search</Button>
     </Col>
   </Row>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,19 @@
+import { useState } from 'react'
 import { useQuery } from 'react-query'
 import LoadingIcon from 'antd/lib/button/LoadingIcon'
 import { CategoriesContext } from '../../store/categories'
 import { fetchCategories } from '../../queries'
 import { Search } from './Search'
+import { Filter } from './Filter'
 
 export const Home = () => {
   const { data, isFetching, isSuccess } = useQuery('categories', fetchCategories)
-
+  const [selectedCategories, setSelectedCategories] = useState([])
   if (isFetching) return <LoadingIcon/>
 
-  if (isSuccess) return <CategoriesContext.Provider value={data}>
+  if (isSuccess) return <CategoriesContext.Provider
+    value={{ allCategories: data, setSelectedCategories, selectedCategories }}>
     <Search/>
+    <Filter/>
   </CategoriesContext.Provider>
 }
