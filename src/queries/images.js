6f4960764594c90a206bbb476ab8257cb7a6cbfa
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-15 11:21:10 +0530

    fixup! feat(queries): add query to fetch array of breeds/sub-breed

--- src/queries/images.js
+++ src/queries/images.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
 
-export const fetchImages = async (breeds) => {
-  console.log(breeds)
-  return await axios.all(breeds.map(breed => axios.get(`${process.env.REACT_APP_API_URL}/breed/${breed}/images`).then((response) => ({
-    breed,
-    images: response.data.message
-  }))))
-}
+export const fetchImages = async (breeds) => await axios.all(breeds.map(breed => axios.get(`${process.env.REACT_APP_API_URL}/breed/${breed}/images`).then((response) => ({
+  breed,
+  images: response.data.message
+}))))
+
 
