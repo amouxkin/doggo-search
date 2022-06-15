d77b8e5669679786df21711c470f407858b5b1ee
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-15 11:18:55 +0530

    feat(pages--Home): add Searchbox

--- src/pages/Home/Search.js
+++ src/pages/Home/Search.js
@@ -1,18 +1,24 @@
-import { Button, Col, Row, Select } from 'antd'
 import { useState } from 'react'
+import { Button, Col, Row, Select } from 'antd'
+import { useQuery } from 'react-query'
+import { fetchImages } from '../../queries'
 import { useCategoriesStore } from '../../store/categories'
 
 export const Search = () => {
   const categories = useCategoriesStore()
   const [value, setValue] = useState([])
+  const { refetch, isFetching } = useQuery('images', async () => await fetchImages(value), { enabled: false })
 
   return <Row justify={'center'}>
     <Col>
-      <Select style={{ width: 300 }} value={value} onChange={setValue} options={categories} mode={'multiple'}
-              placeholder={'Search'} maxTagCount={5}/>
+      <Select style={{ width: 300 }} value={value}
+              onChange={newValues => newValues.length <= 5 && setValue(newValues)}
+              options={categories} mode={'multiple'}
+              placeholder={'Search'}/>
     </Col>
     <Col>
-      <Button disabled={!value.length}>Search</Button>
+      <Button disabled={!value.length || isFetching}
+              onClick={refetch}>Search</Button>
     </Col>
   </Row>
 
