2674d625d585c9829ea63749243fe3421127be8d
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-16 18:00:16 +0530

    feat(pages--home): add FilterState

--- src/pages/Home/Filter.js
+++ src/pages/Home/Filter.js
@@ -4,7 +4,14 @@ import { useQuery } from 'react-query'
 import { useCategoriesStore } from '../../store/categories'
 
 export const Filter = () => {
-  const { allCategories, selectedCategories, setFilterTree, filterTree } = useCategoriesStore()
+  const {
+    allCategories,
+    selectedCategories,
+    setFilterTree,
+    filterTree,
+    filteredCategories,
+    setFilteredCategories
+  } = useCategoriesStore()
   const { data } = useQuery('images')
 
   // get current searched categories
@@ -24,7 +31,7 @@ export const Filter = () => {
       accumulator[breed].children = [
         ...accumulator[breed].children,
         {
-          key: [breed, subBreed].join('-'),
+          key: uniqueSubBreed,
           title: subBreed
         }
       ]
@@ -35,27 +42,40 @@ export const Filter = () => {
     breedsSelected.reduce((accumulator, breed) => {
       if (!accumulator[breed]) accumulator[breed] = {
         title: breed,
-        key: ['0', Object.keys(accumulator).length].join('-'),
+        key: breed,
         children: []
       }
 
       accumulator[breed].children = [
         ...accumulator[breed].children,
-        ...allCategories.filter(breedObject => breedObject.value.split('/')[0] === breed && breedObject.value !== breed).map((subBreed, index) => ({
-          key: [accumulator[breed].key, accumulator[breed].children.length + index].join('-'),
+        ...allCategories.filter(breedObject => breedObject.value.split('/')[0] === breed && breedObject.value !== breed).map((subBreed) => ({
+          key: subBreed.value,
           title: subBreed.value.split('/')[1]
         }))]
 
       return accumulator
     }, uniqueSubBreedFilter)
+
+    uniqueSubBreedFilter['all'] = {
+      key: 'all',
+      title: 'All'
+    }
+
     setFilterTree(uniqueSubBreedFilter)
   }, [data, selectedCategories, allCategories, setFilterTree])
 
-  return <div>
+  return <Tree
+    showLine
+    checkable
+    blockNode
+    defaultExpandedKeys={[]}
+    onCheck={(keys) => {
+      if (keys.includes('all')) {
+        return setFilteredCategories(['all'])
+      }
+      setFilteredCategories(keys)
+    }}
+    checkedKeys={filteredCategories}
+    treeData={Object.values(filterTree || {})}/>
 
-    <Tree showLine
-          blockNode
-          defaultExpandedKeys={['0-0-0']}
-          treeData={Object.values(filterTree || {})}/>
-  </div>
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,16 +5,33 @@ import { CategoriesContext } from '../../store/categories'
 import { fetchCategories } from '../../queries'
 import { Search } from './Search'
 import { Filter } from './Filter'
+import { Col, Row } from 'antd'
 
 export const Home = () => {
   const { data, isFetching, isSuccess } = useQuery('categories', fetchCategories)
   const [selectedCategories, setSelectedCategories] = useState([])
+  const [filteredCategories, setFilteredCategories] = useState([])
   const [filterTree, setFilterTree] = useState()
   if (isFetching) return <LoadingIcon/>
 
   if (isSuccess) return <CategoriesContext.Provider
-    value={{ allCategories: data, setSelectedCategories, selectedCategories, filterTree, setFilterTree }}>
+    value={{
+      allCategories: data,
+      setSelectedCategories,
+      selectedCategories,
+      filterTree,
+      setFilterTree,
+      filteredCategories,
+      setFilteredCategories
+    }}>
     <Search/>
-    <Filter/>
+    <Row>
+      <Col>
+        <Filter/>
+      </Col>
+      <Col>
+
+      </Col>
+    </Row>
   </CategoriesContext.Provider>
 }
